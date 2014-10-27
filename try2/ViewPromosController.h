26ee0bdb4519b118474c3306367327912d3bd7cf
Rosica Boykova <rossi_308@abv.bg>
2014-10-27 11:52:07 -0700

    Project and views made

---
+++ try2/ViewPromosController.h
@@ -0,0 +1,13 @@
+//
+//  ViewPromosController.h
+//  PrImotsiyaApp
+//
+//  Created by admin on 10/27/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewPromosController : UIViewController
+
+@end
